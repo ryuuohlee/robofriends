af80a2e4b6c8768a3061bc795e3004b72ea8f4b4
Jeffrey Lee <jglee1@live.com>
2020-10-13 23:45:51 -0700

    Testing props in react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Hello from './Hello'
+import 'tachyons';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Hello />,
+  <Hello greeting={'Hello' + 'React Ninja'} />,
   document.getElementById('root')
 );
 
