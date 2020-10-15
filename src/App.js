e76ba7a9e803750b1056a61f1322db2e18f399c7
Jeffrey Lee <jglee1@live.com>
2020-10-14 23:03:26 -0700

    Searchbox filters list

--- src/App.js
+++ src/App.js
@@ -12,12 +12,19 @@ class App extends Component {
     }
   }
 
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  }
+
   render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
     return (
       <div className='tc'>
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots}/>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
       </div>
     );
   }
