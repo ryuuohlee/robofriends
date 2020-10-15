82736d690b7c52bfbff226057866eb9581ad2873
Jeffrey Lee <jglee1@live.com>
2020-10-14 22:17:22 -0700

    Created main App component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList'
-import { robots } from './robots';
+import App from './App';
 import 'tachyons';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <CardList robots={robots}/>,
+  <App />,
   document.getElementById('root')
 );
 
