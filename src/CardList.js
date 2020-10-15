db354b9ce1a63854ff87e4755282a6fdaa3c2913
Jeffrey Lee <jglee1@live.com>
2020-10-14 22:10:48 -0700

    Added key prop

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,18 @@ import Card from './Card';
 const CardList = ({ robots }) => {
   return (
     <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      {
+        robots.map((user, i) => {
+          return (
+            <Card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+            />
+          )
+        })
+      }
     </div>
   )
 }
