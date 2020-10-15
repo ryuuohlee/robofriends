b1d01bbcf0c4ae1c3f5a388d0f3fc7aff4aa19fa
Jeffrey Lee <jglee1@live.com>
2020-10-14 21:43:42 -0700

    Added props to card.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import { robots } from './robots';
 import 'tachyons';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Card />,
+  <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />,
   document.getElementById('root')
 );
 
