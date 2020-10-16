5e2959e1c3b0f251c6fe6cab5d83bbb1b0da2348
Jeffrey Lee <jglee1@live.com>
2020-10-15 19:49:37 -0700

    Added loading screen

--- src/App.js
+++ src/App.js
@@ -30,13 +30,18 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots}/>
-      </div>
-    );
+    if(this.state.robots.length === 0) {
+      return <h1>Loading...</h1>
+    }
+    else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filteredRobots}/>
+        </div>
+      );
+    }
   }
 }
 
